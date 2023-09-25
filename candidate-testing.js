2a8aba65b2e152382398553319649f6fbc7a86c4
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-09-24 22:34:16 -0500

    part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,41 +6,45 @@ const input = require('readline-sync');
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-
 let correctAnswer = "Sally Ride";
-
 let candidateAnswer = " ";
+ 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? " , "True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride" , "true" , "40" , "Trajectory" , "3" ];
+let candidateAnswers ;
 
 
 function askForName(candidateName) {
-    console.log("Enter your name: ");
-candidateName = this.candidateName 
+input.askForName("Enter your name: ");
 
   // TODO 1.1b: Ask for candidate's name //
 }
 
 
 function askQuestion(question){
-    console.log("Who was the first American woman in space? Please type her name. ");
-candidateAnswer = this.question
+} for (let i = 0; i < question.length; i++) {
+  candidateAnswers = []; {
+    candidateAnswers.push(question[i]);
+  }
+}  
 
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+//if (candidateAnswer = "(question[input])") {
 
-}
+//////////////////////////////////////////////////// QUESTION HERE ////////////////////////////
+
+
 
 
 // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswer) {
-  if (candidateAnswer == correctAnswer) {
-    console.log("Correct!");
+  if (candidateAnswers === correctAnswer) {
+    console.log( ` ${candidateAnswers}, Correct! `);
   } else { 
-    console.log("Incorrect.") ;
+    console.log(` ${candidateAnswers}, Incorrect.`);
   }
 
 
@@ -49,10 +53,11 @@ function gradeQuiz(candidateAnswer) {
 
   return grade;
 }
+
 function runProgram() {
-  askForName(this.candidateName);
+  askForName();
   // TODO 1.1c: Greet candidate using their name //
-  console.log("\nHello," + this.candidateName + ". Good luck with your quiz!");
+  console.log(`\nHello, ${candidateName}. Good luck with your quiz!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
